3691e8ba146c5718d746c474cf2d704eb383d8f6
Stan Ramsay <ramsaystanley@gmail.com>
2022-12-12 16:25:06 +0000

    Initial Commit

---
+++ Microservice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Microservice
+//
+//  Created by Stanley Ramsay on 12/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
